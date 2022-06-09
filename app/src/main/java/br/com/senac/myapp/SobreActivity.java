d579e99894ee1df3a76df246bf595f9e14a58dec
lndsilva <lndsilva@hotmail.com>
2022-06-09 17:11:03 -0300

    adicionando menu principal

--- app/src/main/java/br/com/senac/myapp/SobreActivity.java
+++ app/src/main/java/br/com/senac/myapp/SobreActivity.java
@@ -3,6 +3,8 @@ package br.com.senac.myapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class SobreActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class SobreActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sobre_layout);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_principal,menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
